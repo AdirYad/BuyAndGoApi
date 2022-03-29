c9dd2002d1073903ef1d9f7fa52eabf35ba48d45
AdirYad <adir.yed@gmail.com>
2022-03-30 00:49:29 +0300

    fixes

--- app/Http/Controllers/FilterCarController.php
+++ app/Http/Controllers/FilterCarController.php
@@ -3,41 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 
 class FilterCarController extends Controller
 {
-    public function __invoke(): array
-    {
-        return [
-            'manufacturers' => $this->distinctManufacturers(),
-            'models' => $this->distinctModels(),
-            'max_monthly_payment' => $this->maxMonthlyPayment(),
-        ];
-    }
-
-    private function maxMonthlyPayment(): int
-    {
-        return Car::select('monthly_payment')->max('monthly_payment') ?? 0;
-    }
-
-    private function distinctManufacturers(): Collection
+    public function __invoke(): Collection
     {
         return Car::query()
-            ->distinct('manufacturer')
-            ->get('manufacturer')
-            ->map(static function (Car $car) {
-                return $car->manufacturer;
-            });
-    }
-
-    private function distinctModels(): Collection
-    {
-        return Car::query()
-            ->distinct('model')
-            ->get('model')
-            ->map(static function (Car $car) {
-                return $car->model;
-            });
+            ->groupBy('manufacturer', 'model')
+            ->orderBy('manufacturer')
+            ->get(['manufacturer', 'model'])
+            ->groupBy('manufacturer');
     }
 }
